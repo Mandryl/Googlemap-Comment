eb8e978a645114125f07d680bc5bdb2ba6c7e064
Kazuki.Sakamoto <kaitoxtennis1997mail@gmail.com>
2022-10-29 22:42:30 +0900

    env init, async add

--- src/Google/place_api.js
+++ src/Google/place_api.js
@@ -1,28 +1,20 @@
 const axios = require("axios");
 const auth = require("./../authentication");
 
-exports.googleMap_placeSearch_api = (landmark) => {
-    console.log(auth.get_googleMaps_env);
+exports.googleMap_placeSearch_api = async(landmark) => {
+    const api_key = auth.get_googleMaps_env().api_key;
     const placeSearch = {
         method: 'get',
-        url: 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json',
+        url: 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?fields=place_id%2Crating',
         params: {
             input: landmark,
             inputtype: "textquery",
-            fields: "place_id,reviews,rating",
-            key: auth.get_googleMaps_env
+            key: api_key
         },
         headers: { }
     };
-    
-    // axios(placeSearch)
-    // .then(function (response) {
-    //     console.log(JSON.stringify(response.data));
-    //     return response.data
-    // })
-    // .catch(function (error) {
-    //     console.log(error);
-    // });
+    const response = await axios(placeSearch);
+    return response.data;
 }
 
 exports.googleMap_placeDetail_api = () => {
--- src/authentication.js
+++ src/authentication.js
@@ -1,11 +1,5 @@
-const env = require('dotenv').config();
-const path  = require('path');
-const currentDir = process.cwd();
-const ENV_PATH = path.join(currentDir, '../.env');
-// dotenv.config({ path: ENV_PATH })
 
 exports.get_googleMaps_env = () => {
-    console.log(path.join(currentDir, '../.env'));
     return {
         api_key: process.env.API_KEY
     }
